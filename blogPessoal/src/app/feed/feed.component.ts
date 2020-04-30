aeeff69385580f08144ffa7200487e0996e3d4c5
Rafaela1997 <rafaelaramosdesigner@gmail.com>
2020-04-29 23:26:43 -0300

    fiz a primeira parte do crud

--- blogPessoal/src/app/feed/feed.component.ts
+++ blogPessoal/src/app/feed/feed.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PostagemService } from '../service/postagem.service';
+import { postagem } from '../model/postagem';
+
 
 @Component({
   selector: 'app-feed',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
-  constructor() { }
+listaPostagens:postagem[]
+postagem: postagem = new postagem
+
+  constructor(private postagemService:PostagemService) { }
 
   ngOnInit(): void {
+    this.findAllPostagens()
   }
 
+findAllPostagens(){
+  this.postagemService.getAllPostagens().subscribe((resp: postagem[])=>{
+this.listaPostagens = resp
+  })
+}
+publicar(){
+  this.postagemService.postPostagem(this.postagem).subscribe((resp: postagem)=>{
+    this.postagem = resp
+   location.assign('/feed')
+  })
+}
 }
--- blogPessoal/src/app/service/postagem.service.ts
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { postagem } from '../model/postagem';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +10,13 @@ export class PostagemService {
 
   constructor(private http: HttpClient) { }
 
-/*CRUD*/
+/*CRUD- create(post),read(get),update(put) e delete(delete)*/
 getAllPostagens(){
   return this.http.get('http://31.220.57.14:8080/postagens')
+}
+
+postPostagem(Postagem: postagem){
+return this.http.post('http://31.220.57.14:8080/postagens', Postagem)
 }
 
 }
